554a46d01ba170d5c35ae69ef4c33ad5636da82b
Heera Rana <heerarana526@gmail.com>
2025-01-31 20:27:24 +0530

    change in sponsers card

--- src/components/Route/Sponsored.jsx
+++ src/components/Route/Sponsored.jsx
@@ -3,27 +3,37 @@ import styles from "../../styles/styles";
 
 const Sponsored = () => {
   return (
-    <div className={`${styles.section} hidden sm:block bg-white py-10 px-5 mb-12 cursor-pointer rounded-xl`}>
-      <div className="flex justify-between w-full">
-        <div className="flex items-start">
-          <img src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736101849/sponser1_ukp0jy.png" style={{width:"250px",paddingTop:"14px", objectFit:"contain"}} alt=""/>
-        </div>
-        <div className="flex items-start">
-          <img src="https://logos-world.net/wp-content/uploads/2020/04/Sony-Logo.png" alt="" style={{width:"150px", objectFit:"contain"}}/>
-        </div>
-        <div className="flex items-start">
-          <img src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736102819/sponser2_xkbcwu.png" style={{width:"158px",paddingTop:"20px", objectFit:"contain"}} alt=""/>
-        </div>
-        <div className="flex items-start">
-          <img src="https://logos-world.net/wp-content/uploads/2020/08/Dell-Logo-1989-2016.png" style={{width:"140px", objectFit:"contain"}} alt="" />
-        </div>
-        <div className="flex items-start">
-          <img src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736102828/sponser3_kki1ex.png"
-            style={{width:"198px",paddingTop:"10px", objectFit:"contain"}} alt=""/>
-        </div>
+    <div className={`${styles.section} bg-white sm:py-10 py-1 px-5 mb-12 cursor-pointer rounded-xl`}>
+      <div className="grid grid-cols-5 sm:grid-cols-3 md:grid-cols-5 sm:gap-6 gap-3 place-items-center">
+        <img 
+          src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736101849/sponser1_ukp0jy.png" 
+          className="h-[60px] sm:h-[60px] object-contain" 
+          alt="Sponsor 1" 
+        />
+        <img 
+          src="https://logos-world.net/wp-content/uploads/2020/04/Sony-Logo.png" 
+          className="h-[50px] sm:h-[60px] object-contain" 
+          alt="Sony" 
+        />
+        <img 
+          src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736102819/sponser2_xkbcwu.png" 
+          className="h-[60px] sm:h-[60px] object-contain" 
+          alt="Sponsor 2" 
+        />
+        <img 
+          src="https://logos-world.net/wp-content/uploads/2020/08/Dell-Logo-1989-2016.png" 
+          className="h-[20px] sm:h-[60px] object-contain" 
+          alt="Dell" 
+        />
+        <img 
+          src="https://res.cloudinary.com/dmvmebkrr/image/upload/v1736102828/sponser3_kki1ex.png" 
+          className="h-[60px] sm:h-[60px] object-contain" 
+          alt="Sponsor 3" 
+        />
       </div>
     </div>
   );
 };
+
 
 export default Sponsored;
