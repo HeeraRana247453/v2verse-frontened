9825bfd74c0471702677cd9342fe0fedb634d283
Heera Rana <heerarana79352@gmail.com>
2025-08-25 12:06:18 +0530

    Change Admin header logo

--- src/components/Admin/Layout/AdminHeader.jsx
+++ src/components/Admin/Layout/AdminHeader.jsx
@@ -13,7 +13,7 @@ const AdminHeader = () => {
          <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
         <Link to="/">
-          <img src="https://shopo.quomodothemes.website/assets/images/logo.svg" alt=""/>
+          <img src="/v2verse.png" alt="V2verse Logo"/>
         </Link>
       </div>
       <div className="flex items-center">
